0d9fa8ac83adc60304048e16283c7fd6a4896e5d
CorinneKelly <corinnemariekelly@gmail.com>
2017-05-13 21:40:57 -0400

    created basic structure, added character counts to state and on change functions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      totalChars: 0,
+      charCount: 0,
+      charsLeft: 0
+    }
+
+    this.handleCharInput = this.handleCharInput.bind(this)    
+    this.handleCharTotal = this.handleCharTotal.bind(this)    
+  }
+
+  handleCharInput(event){
+    event.preventDefault()
+    this.setState({
+      charCount: event.target.value.length,
+      charsLeft: this.state.totalChars - event.target.value.length
+    })
+  }
+
+  handleCharTotal(event){
+    event.preventDefault()
+    this.setState({
+      totalChars: event.target.value.parseInt()
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="">
+        <input type="textbox" className="text-box" onChange={this.handleCharTotal}/>
+        <input type="textarea" className="text-area" onChange={this.handleCharInput} />
+        {this.state.charsLeft}
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
